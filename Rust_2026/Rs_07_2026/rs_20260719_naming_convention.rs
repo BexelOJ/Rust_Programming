e766b2fb1d842072400e672841fd1f843d683039
BexelOJ <bexelo.j@outlook.com>
2026-07-19 23:39:57 +0530

    rust naming convention file added

---
+++ Rust_2026/Rs_07_2026/rs_20260719_naming_convention.rs
@@ -0,0 +1,38 @@
+/*
+
+
+Rust naming conventions:
+
+Item		Naming Convention	Example
+
+crate name	snake_case	    serde, tokio, rand
+
+
+module		snake_case (lowercase)	std, collections, sync
+
+
+function	snake_case		new(), push(), clone()
+
+
+variable	snake_case		user_name, file_path
+
+Trait		PascalCase		Clone, Iterator, Display
+
+
+Struct		PascalCase		Person, HashMap
+
+
+Enum		PascalCase		Option, Result
+
+
+Enum Variant	PascalCase		Some, None, Ok, Err
+
+Constant			SCREAMING_SNAKE_CASE	MAX_SIZE, PI
+
+
+Static Variable		SCREAMING_SNAKE_CASE	GLOBAL_COUNT
+
+
+*/
+
+
